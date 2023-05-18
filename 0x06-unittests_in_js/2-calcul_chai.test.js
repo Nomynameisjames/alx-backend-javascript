d9744844e64950b8bc46ea6affd001dd722ae999
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-19 00:51:18 +0100

    Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,33 +2,32 @@
     assuming a and b are always number
 */
 
-const assert = require('assert');
-
+const chai = require('chai');
 const calculateNumber = require("./1-calcul.js");
 
 describe('calculateNumber type == SUM', () => {
   it('checks the output', () => {
-    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-    assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-    assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
-    assert.strictEqual(calculateNumber('SUM', -1, -1), -2);
+    chai.expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    chai.expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+    chai.expect(calculateNumber('SUM', 1, -1)).to.equal(0);
+    chai.expect(calculateNumber('SUM', -1, -1)).to.equal(-2);
   });
 });
 
 describe('calculateNumber type == SUBTRACT', () => {
   it('checks the output', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 10, 3), 7);
-    assert.strictEqual(calculateNumber('SUBTRACT', 3.1, 2.5), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 2), 3);
-    assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 0), -1);
+    chai.expect(calculateNumber('SUBTRACT', 10, 3)).to.equal(7);
+    chai.expect(calculateNumber('SUBTRACT', 3.1, 2.5)).to.equal(0);
+    chai.expect(calculateNumber('SUBTRACT', 4.5, 2)).to.equal(3);
+    chai.expect(calculateNumber('SUBTRACT', -1.5, 0)).to.equal(-1);
   });
 });
 
 describe('calculateNumber type == DIVIDE', () => {
   it('check the output', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 25, 2.5), 8.333333333333334);
-    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
-    assert.strictEqual(calculateNumber('DIVIDE', 2, 0.0), 'Error');
-    assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    chai.expect(calculateNumber('DIVIDE', 25, 2.5)).to.equal(8.333333333333334);
+    chai.expect(calculateNumber('DIVIDE', 0.0, 2)).to.equal(0);
+    chai.expect(calculateNumber('DIVIDE', 2, 0.0)).to.equal('Error');
+    chai.expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
   });
 });
