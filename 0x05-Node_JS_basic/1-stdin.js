f5b3565c8b87431f306240525316a12c4ca245ac
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-19 19:03:07 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,7 +7,17 @@
  * is now closing (followed by a new line)
  */
 
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+/* const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -22,4 +32,4 @@ rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
 rl.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0);
-});
+}); */
