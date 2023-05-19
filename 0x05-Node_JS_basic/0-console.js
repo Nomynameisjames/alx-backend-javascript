dabc0a3f03f34fffe59924aa660be426accc013d
Nomynameisjames <adavaonimisi@gmail.com>
2023-05-19 13:42:01 +0100

    display message to file stream

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,4 @@
 // displayMessage prints a message to the console
 
-const displayMessage = (message) => {
-  process.stdout.write(message  + '\n');
-};
-
+const displayMessage = (message) => process.stdout.write(`${message}\n`);
 module.exports = displayMessage;
